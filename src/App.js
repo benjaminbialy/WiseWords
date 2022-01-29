68abe70f557bfcdcb11feba641087156d971d184
Benjamin Bialy <benjaminbialy18@gmail.com>
2022-01-29 21:38:02 +1100

    started integrating firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import { db } from "./firebase-config.js"
+import { collection, getDocs } from "firebase/firestore"
 
 function App() {
-  return (
+
+  const [quote, setQuote] = useState([]);
+  const quoteCollection = collection(db, "quotes")
+
+  useEffect(() => {
+
+    const getQuote = async () => {
+      const data = await getDocs(quoteCollection);
+      console.log(data);
+    };
+  
+    return () => {
+      getQuote();
+    };
+  }, []);
+  
+
+  return (    
     <div className="App">
-      <h1>What's going on?</h1>
+      <h1>"{quote}"</h1>
     </div>
   );
 }
---
+++ src/firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "APIKEY",
+  authDomain: "wise-words-6c2ed.firebaseapp.com",
+  projectId: "wise-words-6c2ed",
+  storageBucket: "wise-words-6c2ed.appspot.com",
+  messagingSenderId: "589118638728",
+  appId: "1:589118638728:web:e05f96c5c1aedae8fa2d3d",
+  measurementId: "G-T6C9SCVW2W"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
